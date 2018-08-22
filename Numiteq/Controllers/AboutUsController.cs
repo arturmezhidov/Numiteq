829dc446087e22de2d4dbd6789faea424b7dcff8
Arthur Mezhidov <arthur.mezhidov@gmail.com>
2018-08-22 12:53:02 +0300

    UI.

---
+++ Numiteq/Controllers/AboutUsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Numiteq.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
