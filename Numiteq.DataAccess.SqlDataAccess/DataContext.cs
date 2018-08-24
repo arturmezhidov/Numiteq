8bbcc475b10403ee22a4feddbd24577970607579
Arthur Mezhidov <arthur.mezhidov@gmail.com>
2018-08-24 18:52:30 +0300

    Created DataContext.

---
+++ Numiteq.DataAccess.SqlDataAccess/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Numiteq.Common.Entities;
+
+namespace Numiteq.DataAccess.SqlDataAccess
+{
+    public class DataContext : IdentityDbContext<ApplicationUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
